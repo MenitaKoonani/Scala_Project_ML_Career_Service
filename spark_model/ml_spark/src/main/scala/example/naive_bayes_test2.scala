736848a48b7f7f277255a77b1c4d2a21f4c79607
Sreerag Mandakathil <mandakathil.s@husky.neu.edu>
2019-04-11 12:07:38 -0400

    Edit : Clean up and commenting on the ml files

--- spark_model/ml_spark/src/main/scala/example/naive_bayes_test2.scala
+++ spark_model/ml_spark/src/main/scala/example/naive_bayes_test2.scala
@@ -1,39 +1,57 @@
 package example
 
 import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.ml.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.ml.classification.{NaiveBayes}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{HashingTF, StringIndexer, Tokenizer}
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf}
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 object naive_bayes_test2 extends App {
+
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+  // Defining the configuration for Spark
   val conf = new SparkConf()
   conf.setMaster("local")
-  conf.setAppName("Word Count")
-  val sc = new SparkContext(conf)
+  conf.setAppName("NaiveBayes Text classifier")
 
   val spark = SparkSession
     .builder()
-    .appName("Spark SQL basic example")
+    .appName("NaiveBayes Text classifier")
     .config(conf)
     .getOrCreate()
 
-  val dataFile = "src/main/scala/example/indeed_10-04-2019_cleaned.json" // Should be some file on your system
+  // Specifying the location of the data file
+  val dataFile = "src/main/scala/example/indeed_10-04-2019_cleaned.json"
+
+  // Loading the data file into a data frame
   val df = spark.read.json(dataFile)
 
   val dfSmall = df.select("job_title", "job_posting_desc").where("job_title != 'data analyst'")
+
+  // Defining processes for the pipeline
   val indexer = new StringIndexer().setInputCol("job_title").setOutputCol("label")
   val tokenizer = new Tokenizer().setInputCol("job_posting_desc").setOutputCol("tokens")
   val hashingTF = new HashingTF().setInputCol("tokens").setOutputCol("features")
-
   val nb = new NaiveBayes()
+
+  // Creating a pipeline for the modelling
   val pipeline = new Pipeline().setStages(Array(indexer, tokenizer, hashingTF, nb))
 
+  // Splitting into training and testing data from the main data frame
   val Array(trainingData, testData) = dfSmall.randomSplit(Array(0.7, 0.3), seed = 11L)
+
   println("Testing Data")
   trainingData.show()
   println("Testing Data")
   testData.show()
+
+  // Generating the training model
   val model = pipeline.fit(trainingData)
 
   val prediction = model.transform(testData)
@@ -61,6 +79,6 @@ object naive_bayes_test2 extends App {
 
   val sameModel = PipelineModel.load("src/main/scala/example/spark-model")
 
-  println(f"Accuracy = $accuracy%.4f")
+  println(f"Accuracy = $accuracy%.2f")
   spark.stop()
 }
